35913cdb14ca4c918b866149f509caaabceaac1f
Andrii Davydov <endry.davidko666@gmail.com>
2022-08-01 16:28:10 +0300

    test commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const storeName = 'Andrii';
+const storeDescription = {
+	budget: 10000,
+	employees: ['Worker 1', 'Worker 2', 'Worker 3'],
+	products: {
+		'product-1': {
+			price: 1000,
+		},
+		'product-2': {
+			price: 2000,
+		},
+	},
+	open: true,
+};
+class Rectangle {
+	constructor(height, width) {
+		this.height = height;
+		this.width = width;
+	}
+}
+let arr = [];
+function a() {
+	let asd = 0;
+	return 1;
+}
+console.log(`Мене звати ${storeName}`);
+
+console.log(`Мене звати ${storeName}`);
+console.log('Мене звати ' + storeName);
+console.log('Мене звати ');
+console.log(`Мене звати`);
